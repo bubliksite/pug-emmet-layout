ce0bea3b0b78816dfe22d72310e7c0e0e65095da
bubliksite <serj.solntsev@gmail.com>
2021-01-15 00:24:11 +0300

    Initial commit

---
+++ build/js/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#openProfileMenu')
+const dropdown = document.querySelector('#profileMenu')
+
+button.onclick = function (evt) {
+    evt.preventDefault()
+    dropdown.classList.toggle('d-none')
+}
